ac982b431efb689025125961336a1a7824b0c62b
Tamas Nepusz <ntamas@gmail.com>
2021-07-27 22:03:10 +0200

    fix: show proto has a single-cell battery

--- app/conf/show-proto-v2.mk
+++ app/conf/show-proto-v2.mk
@@ -10,7 +10,3 @@ CF_MODEL_POSITIONING=lighthouse
 # Configure ESC and start disarmed; use some idle thrust as well
 CFLAGS += -DENABLE_ONESHOT125 -DSTART_DISARMED -DDEFAULT_IDLE_THRUST=5000
 
-## The low voltage thresholds have to be tuned here, these are simply obtained
-## by multiplying whatever we've had with 2 since the new show drone prototype
-## used two cells.
-CFLAGS += -DLOW_VOLTAGE=6.4f -DCRITICAL_LOW_VOLTAGE=6.0f
