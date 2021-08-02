7161e49ffd752e4eee855f8cf1e95f45424da08d
Tamas Nepusz <ntamas@gmail.com>
2021-08-02 18:27:40 +0200

    fix: don't disarm the motors for the show proto v2 by default

--- app/conf/show-proto-v2.mk
+++ app/conf/show-proto-v2.mk
@@ -2,11 +2,14 @@
 # instead of UWB
 
 # Mass of the show drone prototype is about 100g
-CF_MODEL_MASS=0.1f  # TODO(ntamas): measure once we have it
+CF_MODEL_MASS=0.1f
 
 # Use Lighthouse positioning
 CF_MODEL_POSITIONING=lighthouse
 
-# Configure ESC and start disarmed; use some idle thrust as well
-CFLAGS += -DENABLE_ONESHOT125 -DSTART_DISARMED -DDEFAULT_IDLE_THRUST=5000
+# Configure ESC
+CFLAGS += -DENABLE_ONESHOT125
+
+# Start disarmed; use some idle thrust as well. Enable this if needed.
+# CFLAGS += -DSTART_DISARMED -DDEFAULT_IDLE_THRUST=5000
 
